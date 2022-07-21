047cfca9b4f4caac7152da9276333f3f3f0bc300
Kamil A. Khan <kamil@alum.mit.edu>
2022-07-20 23:19:17 -0400

    move examples into functions

--- src/AbsNormal.jl
+++ src/AbsNormal.jl
@@ -4,29 +4,6 @@ import JuMP, PATHSolver
 
 using LinearAlgebra
 
-## solve the test problem listed at https://github.com/chkwon/PATHSolver.jl
-
-#=
-const M = [0 0 -1 -1;
-           0 0 1 -2;
-           1 -1 2 -2;
-           1 2 -2 4]
-
-const q = [2, 2, -2, -6]
-
-model = JuMP.Model(PATHSolver.Optimizer)
-JuMP.set_optimizer_attribute(model, "output", "no")
-JuMP.@variable(model, x[1:4] >= 0)
-JuMP.@constraint(model, complements(M*x + q, x))
-
-JuMP.optimize!(model)
-@show JuMP.termination_status(model)
-@show JuMP.value.(x)
-# @show JuMP.solution_summary(model) # apparently not yet implemented
-=#
-
-## solve absolute value equations via CANF
-
 struct AnfCoeffs
     c::Vector{Float64}
     b::Vector{Float64}
@@ -43,14 +20,14 @@ end
 
 function modify_anf_coeffs(a::AnfCoeffs)
     IMinusL = I - a.L
-    (cm, Lm, Zm) = map(X -> IMinusL\X, (a.c, I+a.L, a.Z))
+    (cm, Zm, Lm) = map(X -> IMinusL\X, (a.c, a.Z, I+a.L))
     bm = a.b + a.Y*cm
     Ym = a.Y*(I + Lm)
     Jm = a.J + a.Y*Zm
     return AnfCoeffs(cm, bm, Zm, Lm, Jm, Ym)
 end
 
-function solve_pa_equation(a::AnfCoeffs, approach::EquationSolvingApproach = BY_MLCP)
+function solve_pa_equation(a::AnfCoeffs; approach::EquationSolvingApproach = BY_MLCP)
     aMod = modify_anf_coeffs(a::AnfCoeffs)
     cm = aMod.c
     bm = aMod.b
@@ -63,8 +40,8 @@ function solve_pa_equation(a::AnfCoeffs, approach::EquationSolvingApproach = BY_
     JuMP.set_optimizer_attribute(anfModel, "output", "no")
 
     if approach == BY_MLCP
-        JuMP.@variable(anfModel, x[1:size(Z)[2]])
-        JuMP.@variable(anfModel, u[1:length(c)] >= 0.0)
+        JuMP.@variable(anfModel, x[1:size(Zm)[2]])
+        JuMP.@variable(anfModel, u[1:length(cm)] >= 0.0)
 
         # JuMP.@constraint(anfModel, bm + Jm*x + Ym*u .== 0.0) # not supported by PATHSolver.jl
         JuMP.@constraint(anfModel, complements(bm + Jm*x + Ym*u, x))
@@ -77,9 +54,9 @@ function solve_pa_equation(a::AnfCoeffs, approach::EquationSolvingApproach = BY_
     elseif approach == BY_LCP
         ZJmInv = Zm/Jm
         gamma = cm - ZJmInv*bm
-        S = Lm - ZJmInv*Y
+        S = Lm - ZJmInv*Ym
 
-        JuMP.@variable(anfModel, u[1:length(c)] >= 0.0)
+        JuMP.@variable(anfModel, u[1:length(cm)] >= 0.0)
 
         JuMP.@constraint(anfModel, complements(gamma + S*u, u))
 
@@ -95,63 +72,64 @@ function solve_pa_equation(a::AnfCoeffs, approach::EquationSolvingApproach = BY_
     return xStar, terminationStatus
 end
 
-# solve the equation system:
-# 0 = 1 + x - abs(x) + abs(x + abs(x))
-function example_1()
-    const c = [0.0, 0.0]
-    const b = [1.0]
-    const Z = [1.0; 1.0;;]
-    const L = LowerTriangular([0.0 0.0; 1.0 0.0])
-    const J = [1.0;;]
-    const Y = [-1.0 1.0]
+# test PATHSolver.jl using its documented example
+function test_PATH()
+    M = [0 0 -1 -1;
+         0 0 1 -2;
+         1 -1 2 -2;
+         1 2 -2 4]
 
-    anf = anfConstants(b, c, Z, L, Y, J)
+    q = [2, 2, -2, -6]
+
+    model = JuMP.Model(PATHSolver.Optimizer)
+    JuMP.set_optimizer_attribute(model, "output", "no")
+    
+    JuMP.@variable(model, x[1:4] >= 0)
+    
+    JuMP.@constraint(model, complements(M*x + q, x))
+
+    JuMP.optimize!(model)
+    
+    @show JuMP.termination_status(model)
+    @show JuMP.value.(x)
+    # @show JuMP.solution_summary(model) # apparently not yet implemented
+end
+
+# solve the equation system:
+#   0 = 1 + x - abs(x) + abs(x + abs(x))
+function example_1()
+    c = [0.0, 0.0]
+    b = [1.0]
+    Z = [1.0; 1.0;;]
+    L = LowerTriangular([0.0 0.0; 1.0 0.0])
+    J = [1.0;;]
+    Y = [-1.0 1.0]
+
+    anf = AnfCoeffs(c, b, Z, L, J, Y)
 
     xStar, terminationStatus = solve_pa_equation(anf)
 
     @show xStar
     @show terminationStatus
 end
-    
-#=
-IMinusL = I - L
-(cm, Lm, Zm) = map(X -> IMinusL\X, (c, I+L, Z))
-bm = b + Y*cm
-Ym = Y*(I + Lm)
-Jm = J + Y*Zm
 
-ZJmInv = Zm/Jm
-gamma = cm - ZJmInv*bm
-S = Lm - ZJmInv*Y
+# solve the equation system:
+#   0 = 1 + x + 0.5*abs(x) - 0.5*abs(x)
+function example_2()
+    c = [0.0, 0.0]
+    b = [1.0]
+    Z = [1.0; 1.0;;]
+    L = LowerTriangular(zeros(2, 2))
+    J = [1.0;;]
+    Y = [0.5 -0.5]
 
-anfModel = JuMP.Model(PATHSolver.Optimizer)
-JuMP.set_optimizer_attribute(anfModel, "output", "no")
+    anf = AnfCoeffs(c, b, Z, L, J, Y)
 
-JuMP.@variable(anfModel, x[1:size(Z)[2]])
-JuMP.@variable(anfModel, u[1:length(c)] >= 0.0)
-# JuMP.@variable(anfModel, dummyVar[1:length(b)])
+    xStar, terminationStatus = solve_pa_equation(anf)
 
-# JuMP.@constraint(anfModel, bm + Jm*x + Ym*u .== 0.0) # not supported by PATHSolver.jl
-JuMP.@constraint(anfModel, complements(bm + Jm*x + Ym*u, x))
-JuMP.@constraint(anfModel, complements(cm + Zm*x + Lm*u, u))
+    @show xStar
+    @show terminationStatus
+end
 
-JuMP.optimize!(anfModel)
-
-@show JuMP.termination_status(anfModel)
-@show JuMP.value.(x)
-=#
-
-#=
-JuMP.@variable(anfModel, u[1:length(c)] >= 0.0)
-
-JuMP.@constraint(anfModel, complements(gamma + S*u, u))
-
-JuMP.optimize!(anfModel)
-
-@show JuMP.termination_status(anfModel)
-uStar = JuMP.value.(u)
-xStar = -Jm\(bm + Ym*uStar)
-@show xStar
-=#
-
-# end # module
+example_1()
+example_2()
